10bf64cb30f60881e7dca90df65ccffd43d8cb81
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-04 16:38:20 +0300

    lang-syntax: task 2

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,22 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+
+        number = Math.abs(number);
+
+        int res = 0;
+
+        for (int i = 0; i < 4; i++) {
+            int temp = number % 10;
+            res += temp;
+            number = (number - temp) / 10;
+        }
+
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) {
+        new DigitsSumCalculator().calculateSum(-1111);
     }
 
 }
