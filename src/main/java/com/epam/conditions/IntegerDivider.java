fe9e9ee3b6a98a0029da19084a1fb9e35a131244
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-04 21:52:15 +0300

    conditions: task 4

--- src/main/java/com/epam/conditions/IntegerDivider.java
+++ src/main/java/com/epam/conditions/IntegerDivider.java
@@ -3,6 +3,25 @@ package com.epam.conditions;
 public class IntegerDivider {
 
     public void printCompletelyDivided(int dividend, int divider) {
+
+        if (divider == 0) {
+            System.out.println("division by zero");
+            return;
+        }
+
+        int res = dividend / divider;
+
+        if (res * divider == dividend) {
+            System.out.println("can be divided completely");
+        } else {
+            System.out.println("cannot be divided completely");
+        }
+    }
+
+    public static void main(String[] args) {
+        new IntegerDivider().printCompletelyDivided(10, 0);
+        new IntegerDivider().printCompletelyDivided(10, 3);
+        new IntegerDivider().printCompletelyDivided(10, 2);
     }
 
 }
