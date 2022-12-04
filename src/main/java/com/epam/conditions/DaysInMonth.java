1c963431a84a0896543314b980de701353f9855c
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-04 17:46:13 +0300

    conditions: task 3

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,7 +3,31 @@ package com.epam.conditions;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+        }
+
+        if (month % 2 == 0) {
+            System.out.println(30);
+        } else {
+            System.out.println(31);
+        }
+
+        boolean leap =
+                (year % 4 == 0
+                && year % 100 != 0)
+                || (year % 4 == 0
+                && year % 100 == 0
+                && year % 400 == 0);
+
+        if (month == 2) {
+            System.out.println(leap ? 29 : 28);
+        }
+
 
     }
 
+    public static void main(String[] args) {
+        new DaysInMonth().printDays(2000, 2);
+    }
 }
