67d9a04f9296e331434a860a96fc078a418d808e
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-04 21:59:03 +0300

    conditions: task 6

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -3,6 +3,14 @@ package com.epam.conditions;
 public class BitwiseValuesSwap {
 
     public void swap(int first, int second) {
+        first = first ^ second;
+        second = first ^ second;
+        first = first ^ second;
+        System.out.println(first + " " + second);
+    }
+
+    public static void main(String[] args) {
+        new BitwiseValuesSwap().swap(7, 53);
     }
 
 }
