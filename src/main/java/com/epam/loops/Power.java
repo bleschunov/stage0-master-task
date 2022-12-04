cf3c2a605fe0d22347e261e2087422cbcafcde70
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-04 22:16:41 +0300

    loops: task 3

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -3,11 +3,18 @@ package com.epam.loops;
 public class Power {
 
     public void printPower(int numberToPrint, int power) {
+        int res = 1;
 
+        for (int i = 0; i < power; i++) {
+            res *= numberToPrint;
+        }
+
+        System.out.println(res);
     }
 
     public static void main(String[] args) {
         new Power().printPower(10, 3);
+        new Power().printPower(2, 3);
     }
 
 }
