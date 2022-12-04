067569918579386e07f0f470b2060cc15678f0a5
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-04 21:57:29 +0300

    conditions: task 5

--- src/main/java/com/epam/conditions/CoordinatePane.java
+++ src/main/java/com/epam/conditions/CoordinatePane.java
@@ -3,6 +3,30 @@ package com.epam.conditions;
 public class CoordinatePane {
 
     public void printQuadrant(int x, int y) {
+
+        if (x == 0 || y == 0) {
+            System.out.println("zero");
+            return;
+        }
+
+        if (x > 0 && y > 0) {
+            System.out.println("first");
+        } else if (x < 0 && y < 0) {
+            System.out.println("third");
+        } else if (x > 0 && y < 0) {
+            System.out.println("fourth");
+        } else if (x < 0 && y > 0) {
+            System.out.println("second");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new CoordinatePane().printQuadrant(1, 1);
+        new CoordinatePane().printQuadrant(1, -1);
+        new CoordinatePane().printQuadrant(-1, -1);
+        new CoordinatePane().printQuadrant(-1, 1);
+        new CoordinatePane().printQuadrant(1, 0);
     }
 
 }
