2bb96bc9f27b9cd2257d44778300861238280b4c
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-04 16:53:26 +0300

    conditions: task 2

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -2,8 +2,23 @@ package com.epam.conditions;
 
 public class TriangleSidesValidator {
 
-    public void validate(double firstSide, double secondSide, double thirdSide) {
+    private final static String NOT_TRIANGLE = "it's not a triangle";
+    private final static String TRIANGLE = "this is a valid triangle";
 
+    public void validate(double firstSide, double secondSide, double thirdSide) {
+        if (    firstSide + secondSide > thirdSide
+                && secondSide + thirdSide > firstSide
+                && thirdSide + firstSide > secondSide) {
+            System.out.println(TRIANGLE);
+            return;
+        }
+
+        System.out.println(NOT_TRIANGLE);
+    }
+
+    public static void main(String[] args) {
+        new TriangleSidesValidator().validate(3, 4, 5);
+        new TriangleSidesValidator().validate(1, 1, 5);
     }
 
 }
