d7d1b4b6f5051def66f87a97f69a43aa5dd59f0a
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-04 16:34:03 +0300

    lang-syntax: task 1

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,11 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        System.out.println(new StringBuilder(String.valueOf(number)).reverse());
+    }
+
+    public static void main(String[] args) {
+        new NumberReverter().revert(489);
     }
 
 }
