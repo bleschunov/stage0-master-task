2d1cf7a6352edeebce488851e93d6a67d433f132
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-04 22:19:31 +0300

    loops: task 4

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -4,10 +4,19 @@ public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
 
+        StringBuilder sb = new StringBuilder("*");
+
+        for (int i = 0; i < cathetusLength; i++) {
+            System.out.printf("%" + cathetusLength + "s", sb);
+            sb.append("*");
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        new HalfPyramid().printHalfPyramid(8);
+//        new HalfPyramid().printHalfPyramid(8);
+//        new HalfPyramid().printHalfPyramid(2);
+        new HalfPyramid().printHalfPyramid(3);
     }
 
 }
