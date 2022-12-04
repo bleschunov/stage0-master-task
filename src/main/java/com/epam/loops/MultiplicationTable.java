284461adda3948831d48823a244961894e1c8b66
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-12-04 22:12:53 +0300

    loops: task 1

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -3,6 +3,34 @@ package com.epam.loops;
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
+
+        System.out.printf("%4s ", " ");
+
+        for (int i = 1; i <= numberTableToPrint; i++) {
+            System.out.printf("%3d ", i);
+        }
+
+        System.out.println();
+        System.out.printf("%4s ", "  ");
+
+        for (int i = 1; i <= numberTableToPrint; i++) {
+            System.out.printf("%3s ", "-");
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= numberTableToPrint; i++) {
+            System.out.printf("%3d %1s", i, "|");
+            for (int j = 1; j <= numberTableToPrint; j++) {
+                System.out.printf("%3d ", i * j);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new MultiplicationTable().printTable(13);
     }
 
 }
